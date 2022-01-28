395b022f992b665fbba586507e2d81602a5db7ba
AndaLuiza <anda.turcanu@gmail.com>
2022-01-28 21:41:13 +0200

    - maven project structure
    - spring boot
    - spring web
    - spring security
    - spring data jpa
    - h2 in memory db
    - lombok

---
+++ src/test/java/com/spring/user/authentication/SpringUserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.user.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
